aac087afcc9f0338fe95ea00397acfcb425feadd
雄大小寺 <aria@kotera-mbp.local>
2018-05-07 17:06:17 +0900

    localstrageに対応

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,29 @@
 $(function (){
+    // window.localStorage.getItem('todo');
+    // window.localStorage.setItem('todo', '');
+    getLiItems();
+    
     $("#add-btn").on("click",()=>{
             if($("#message-txt").val() !== ""){
             $(".todo-list").prepend(`<li class="li-list">
             <p>${$("#message-txt").val()}</p>
             <button class="del-li">delete</button> </li>`);
+            setLiItems();
         }
     });
+    $("#allclear-btn").on("click",()=>{
+        $(".todo-list").empty();
+        setLiItems();
+    });
+
 
 
     $(".todo-list").on("click",".del-li",(event)=>{
-        
         $(event.currentTarget).parent().remove();
+        setLiItems();
     });
+    
+
     $(".todo-list").on("click",".li-list p",(event)=>{
         let textF = $(event.currentTarget).parent().children("p").html();        
         let liEl = $(event.currentTarget).parent();
@@ -22,6 +34,7 @@ $(function (){
         <button class="modify-li">change</button>
         <button class="del-li">del</button> 
         `);
+        setLiItems();
     });
     $(".todo-list").on("click",".modify-li",(event)=>{
         console.log($(event.currentTarget).parent())
@@ -36,5 +49,33 @@ $(function (){
         if(textF===""){
             liEl.remove();
         }
+        setLiItems();
     });
+
 });
+function setLiItems(){
+    let array =[];
+    $(".li-list").children();
+    for(let text of $(".li-list").children("p")){
+        console.log(text);
+        console.log(text.textContent);
+        array.push(text.textContent);
+    }
+    console.log(array);
+    window.localStorage.setItem('todo', array.toString());
+}
+function getLiItems(){
+    var str = window.localStorage.getItem('todo');
+
+    
+    if(str !== null || str !== ""){
+        let array = str.split(",");
+        console.log(array);
+        for(let text of array.reverse()){
+            $(".todo-list").prepend(`<li class="li-list">
+            <p>${text}</p>
+            <button class="del-li">delete</button> </li>`);
+        }
+    }   
+    
+}
